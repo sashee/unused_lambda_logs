9375ce80bfebcc387195983c3ca95e2ad278f13d
Tamas Sallai <gsashee@gmail.com>
2019-05-27 12:21:15 +0200

    cleanup

--- script.js
+++ script.js
@@ -7,34 +7,32 @@ const EMPTY = Symbol("empty");
 	const regions = (await ec2.describeRegions().promise()).Regions.map(({RegionName}) => RegionName).filter((region) => region === "eu-west-1" || region === "us-east-1" || true);
 	const lambdas = await Promise.all(regions.map(async (region) => {
 		const lambda = new AWS.Lambda({region});
-		async function* fetchLambdas() {
+		const res = [];
+		for await (const lf of (async function* fetchLambdas() {
 			let NextMarker = EMPTY;
 			while (NextMarker || NextMarker === EMPTY) {
 				const functions = await lambda.listFunctions({Marker: NextMarker !== EMPTY ? NextMarker : undefined}).promise();
 				yield* functions.Functions.map(({FunctionName}) => FunctionName);
 				NextMarker = functions.NextMarker;
 			}
-		}
-
-		const res = [];
-		for await (const lf of fetchLambdas()) {
+		})()) {
 			res.push(lf);
 		}
+
 		return {region, functions: res};
 	}));
 
 	const logGroups = await Promise.all(regions.map(async (region) => {
 		const logs = new AWS.CloudWatchLogs({region});
-		async function* fetchLogGroups() {
+		const res = [];
+		for await (const lg of (async function* fetchLogGroups() {
 			let nextToken = EMPTY;
 			while (nextToken || nextToken === EMPTY) {
 				const logGroups = await logs.describeLogGroups({nextToken: nextToken !== EMPTY ? nextToken : undefined, logGroupNamePrefix: "/aws/lambda/"}).promise();
 				yield* logGroups.logGroups.map(({logGroupName, storedBytes, retentionInDays}) => ({logGroupName, storedBytes, retentionInDays}));
 				nextToken = logGroups.nextToken;
 			}
-		}
-		const res = [];
-		for await (const lg of fetchLogGroups()) {
+		})()) {
 			res.push(lg);
 		}
 
