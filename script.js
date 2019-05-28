4a5a1fd38cc8ebc145b77984a2d40e42603fb66f
Tamas Sallai <gsashee@gmail.com>
2019-05-28 16:24:41 +0200

    speedup

--- script.js
+++ script.js
@@ -21,32 +21,35 @@ const getPaginatedResults = async (fn) => {
 
 (async () => {
 	const regions = (await ec2.describeRegions().promise()).Regions.map(({RegionName}) => RegionName);
-	const lambdas = await Promise.all(regions.map(async (region) => {
-		const lambda = new AWS.Lambda({region});
-		const res = await getPaginatedResults(async (NextMarker) => {
-			const functions = await lambda.listFunctions({Marker: NextMarker}).promise();
-			return {
-				marker: functions.NextMarker,
-				results: functions.Functions.map(({FunctionName}) => FunctionName),
-			};
-		});
+	const [lambdas, logGroups] = await Promise.all([
+		// get all lambdas
+		Promise.all(regions.map(async (region) => {
+			const lambda = new AWS.Lambda({region});
+			const res = await getPaginatedResults(async (NextMarker) => {
+				const functions = await lambda.listFunctions({Marker: NextMarker}).promise();
+				return {
+					marker: functions.NextMarker,
+					results: functions.Functions.map(({FunctionName}) => FunctionName),
+				};
+			});
 
-		return {region, functions: res};
-	}));
+			return {region, functions: res};
+		})),
+		// get all log groups
+		Promise.all(regions.map(async (region) => {
+			const logs = new AWS.CloudWatchLogs({region});
+			const res = await getPaginatedResults(async (NextMarker) => {
+				const logGroups = await logs.describeLogGroups({nextToken: NextMarker, logGroupNamePrefix: "/aws/lambda/"}).promise();
+				return {
+					marker: logGroups.nextToken,
+					results: logGroups.logGroups.map(({logGroupName, storedBytes, retentionInDays}) => ({logGroupName, storedBytes, retentionInDays})),
+				};
 
-	const logGroups = await Promise.all(regions.map(async (region) => {
-		const logs = new AWS.CloudWatchLogs({region});
-		const res = await getPaginatedResults(async (NextMarker) => {
-			const logGroups = await logs.describeLogGroups({nextToken: NextMarker, logGroupNamePrefix: "/aws/lambda/"}).promise();
-			return {
-				marker: logGroups.nextToken,
-				results: logGroups.logGroups.map(({logGroupName, storedBytes, retentionInDays}) => ({logGroupName, storedBytes, retentionInDays})),
-			};
+			});
 
-		});
-
-		return {region, logGroups: res};
-	}));
+			return {region, logGroups: res};
+		}))
+	]);
 
 	const unusedLogGroups = logGroups.map(({region, logGroups}) => {
 		const unusedLogGroups = logGroups.filter(({logGroupName}) => {
